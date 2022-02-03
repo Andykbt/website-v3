491f0e019f6a1f710a90cb09545ce67d4a36a1af
Andykbt <andy.truong2001@gmail.com>
2022-02-04 01:21:35 +1100

    initial commit

---
+++ web/components/sections/footer/footer-styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { colourDarkGrey } from "@website-v3/web/styles";
+
+export const FooterContainer = styled.div`
+  border-top: solid 1px ${colourDarkGrey}
+`;
--- web/components/sections/index.ts
+++ web/components/sections/index.ts
@@ -2,3 +2,4 @@ export * from "./about";
 export * from "./experience";
 export * from "./projects";
 export * from "./skills";
+export * from "./footer";
