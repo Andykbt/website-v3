fa788e4d95dd6873fae1547045bd5520dfb6cf3a
Andykbt <andy.truong2001@gmail.com>
2022-02-05 02:18:02 +1100

    added sitemap schema

--- studio/schemas/documents/index.ts
+++ studio/schemas/documents/index.ts
@@ -1,3 +1,4 @@
 export * from "./blog";
 export * from "./project";
 export * from "./experience";
+export * from "./sitemap";
--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -4,17 +4,18 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import { project, experience, blog } from "@website-v3/studio/schemas/documents";
+import { project, experience, blog, sitemap } from "@website-v3/studio/schemas/documents";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-    // We name our schema
-    name: "default",
-    // Then proceed to concatenate our document type
-    // to the ones provided by any plugins that are installed
-    types: schemaTypes.concat([
-        project,
-        blog,
-        experience,
-    ]),
+  // We name our schema
+  name: "default",
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    project,
+    blog,
+    experience,
+    sitemap,
+  ]),
 });
