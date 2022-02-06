5a20bd09cd64d154fe3846d2e4b0a4d392043037
Andykbt <andy.truong2001@gmail.com>
2022-02-07 00:59:23 +1100

    added tests

--- web/components/header/header.test.tsx
+++ web/components/header/header.test.tsx
@@ -1,22 +1,56 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Url } from "@website-v3/web/constants/types";
-import { Header } from "./header";
+import { Header } from "@website-v3/web/components";
+import { createMockRouter } from "../../helpers/test-utils/createMockRouter";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+
 
 describe("header", () => {
-  it("displays the correct nav items", () => {
-    const mockItems: Url[] = [
-      {
-        name: "mockNavItem",
-        url: ""
-      }
-    ];
+  const mockItems: Url[] = [
+    {
+      name: "mockNavItem",
+      url: "/mock"
+    },
+    {
+      name: "mockNavItem1",
+      url: "/mock1",
+    }
+  ];
 
-    const { getByText } = render(<Header navItems={mockItems} />);
+  it("displays the correct nav items", () => {
+    const router = createMockRouter({});
+    const { getByText, getAllByRole } = render(
+      <RouterContext.Provider value={router}>
+        <Header navItems={mockItems} />
+      </RouterContext.Provider>  
+    );
+    const nav1 = getByText("mockNavItem");
+    const nav2 = getByText("mockNavItem1");
+
+    expect(getAllByRole("header.navItems").length).toBe(2);
     expect(getByText("mockNavItem")).toBeInTheDocument();
+    expect(getByText("mockNavItem1")).toBeInTheDocument();
+
+    fireEvent.click(nav1);
+    fireEvent.click(nav2);
+
+    expect(router.push).toHaveBeenCalledWith(mockItems[0].url);
+    expect(router.push).toHaveBeenCalledWith(mockItems[1].url);
   });
 
-  // it("goes to the correct url", () => {
-      
-  // });
+  it("home button redirects to homepage", () => {
+    const router = createMockRouter({});
+    const { getByTestId } = render(
+      <RouterContext.Provider value={router}>
+        <Header navItems={mockItems} />
+      </RouterContext.Provider>  
+    );
+
+    const homeButton = getByTestId("header.home-button");
+    expect(homeButton).toBeInTheDocument();
+    
+    fireEvent.click(homeButton);
+    expect(router.push).toHaveBeenCalledWith("/");
+  });
 });
--- web/components/header/header.tsx
+++ web/components/header/header.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
 import React, { useState } from "react";
 import { Url } from "../../constants/types";
 import { CircleSvg } from "../../styles/svg/Circle-svg";
 import { NavItem, Nav } from "./header-styled";
+import { useRouter } from "next/router";
 
 type HeaderProps = {
 	navItems: Url[],
@@ -11,24 +11,30 @@ type HeaderProps = {
 export const Header = ({
   navItems,
 }: HeaderProps) => {
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
 
   const renderNavItems = () => {
     return navItems.map((item, index) =>
-      <NavItem key={index} href={item.url}>
+      <NavItem
+        key={index}
+        href={item.url}
+        onClick={() => router.push(item.url)}
+        role="header.navItems"
+      >
         {item.name}
       </NavItem>
     );
   };
-  
-  const [progress, setProgress] = useState(0);
 
   return (
     <Nav>
       <a
-        href={"/"}
         onMouseEnter={() => setProgress(100)}
-        onMouseLeave={() => setProgress(0)}>
-        <CircleSvg radius={25} stroke={1} progress={progress}>{"N D"}</CircleSvg>
+        onMouseLeave={() => setProgress(0)}
+        onClick={() => router.push("/")}
+        data-testid="header.home-button">
+        <CircleSvg radius={25} stroke={2} progress={progress}>{"N D"}</CircleSvg>
       </a>
       {renderNavItems()}
     </Nav>
