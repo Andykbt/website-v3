8007676bb59bbbe719037894232bbd19df28008d
Andy Truong <55374347+Andykbt@users.noreply.github.com>
2022-02-16 22:50:06 +1100

    [WEB-41] Create hero component (#24)
    * Added hero component
    
    * added styles
    
    * fixed imports

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,14 +6,8 @@ import {
   About,
   Experience,
   Footer,
+  Hero
 } from "@website-v3/web/components";
-import {
-  Body1,
-  H1,
-} from "@website-v3/web/styles";
-import { fontSizeExtraLarge, fontSizeSmall } from "@website-v3/web/styles";
-import { TextTrail } from "@website-v3/web/helpers/springs";
-import { useInView } from "react-intersection-observer";
 import { SanityClient } from "../sanity";
 
 type Props = {
@@ -25,17 +19,9 @@ const Home: NextPage<Props> = ({
   projects,
   experience
 }: Props) => {
-  const { ref, inView } = useInView();
   return (
     <>
-      <div style={{margin: "5vh auto 0 5vw"}} ref={ref}>
-        <TextTrail on={inView}>
-          <H1 fontSize={fontSizeExtraLarge}>Andy</H1>
-          <br />
-          <H1 fontSize={fontSizeExtraLarge}>&nbsp;&nbsp;&nbsp;&nbsp;Truong</H1>
-        </TextTrail>
-      </div>
-      <Body1 fontSize={fontSizeSmall} textDirection="right" margin="7.5vh 20vw">SOFTWARE<br/>ENG</Body1>
+      <Hero/>
       <About/>
       <Experience experiences={experience}/>
       <Skills pages={5} />
