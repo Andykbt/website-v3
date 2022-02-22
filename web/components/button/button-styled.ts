76714dfb498954ba2ccc5c8fd2804ec0dc325d1d
Andy Truong <55374347+Andykbt@users.noreply.github.com>
2022-02-22 21:15:16 +1100

    [WEB-45] Revamp/refactor of projects page (#28)
    * types, props, styles
    
    * new card component
    
    * back button and spring changes
    
    * fixed imports and debugs

--- web/components/button/button-styled.ts
+++ web/components/button/button-styled.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import { colourDarkGrey } from "../../styles";
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<{background: string | undefined}>`
   padding: 25px;
   outline: none;
   border: none;
   border-radius: 9999px;
   border: solid 2px ${colourDarkGrey};
-  background: transparent;
+  background: ${props => props.background ? props.background : "transparent"};
   cursor: pointer;
 `;
--- web/components/index.ts
+++ web/components/index.ts
@@ -2,3 +2,4 @@ export * from "./header";
 export * from "./layout";
 export * from "./sections";
 export * from "./button";
+export * from "./card";
