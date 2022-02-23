edb86eaa520b875260e4bcf5940f17405ed06dd3
Andykbt <andy.truong2001@gmail.com>
2022-02-23 22:31:45 +1100

    project page header and types

--- studio/schemas/documents/project.ts
+++ studio/schemas/documents/project.ts
@@ -1,69 +1,102 @@
 export const project = {
-    name: "project",
-    title: "Projects",
-    type: "document",
-    fields: [
+  name: "project",
+  title: "Projects",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Project name",
+      type: "string",
+    },
+    {
+      name: "date",
+      title: "Date",
+      description: "Write when your project was published",
+      type: "date",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96
+      }
+    },
+    {
+      name: "nextProject",
+      title: "Next project",
+      type: "reference",
+      to: [{ type: "project" }],
+    },
+    {
+      name: "excerpt",
+      title: "Excerpt",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "image",
+      title: "Project Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
         {
-            name: "title",
-            title: "Project name",
-            type: "string",
+          name: "caption",
+          title: "Caption",
+          type: "string",
         },
         {
-            name: "slug",
-            title: "Slug",
-            type: "slug",
-            options: {
-                source: "title",
-                maxLength: 96
-            }
-        },
-        {
-            name: "excerpt",
-            title: "Excerpt",
-            type: "array",
-            of: [{ type: "block" }],
-        },
-        {
-            name: "image",
-            title: "Project Image",
-            type: "image",
-            options: {
-                hotspot: true,
-            },
-            fields: [
-                {
-                    name: "caption",
-                    title: "Caption",
-                    type: "string",
-                },
-                {
-                    name: "alt",
-                    title: "Alt",
-                    type: "string",
-                }
-            ]
-        },
-        {
-            name: "codeLink",
-            title:"Code Link",
-            type: "url",
-        },
-        {
-            name: "projectLink",
-            title:"Project Link",
-            type: "url",
-        },
-        {
-            name: "body",
-            title: "Body",
-            type: "array",
-            of: [{ type: "block" }]
-        },
-        {
-            name: "keywords",
-            title: "Keywords",
-            type: "array",
-            of: [{ type: "string" }],
+          name: "alt",
+          title: "Alt",
+          type: "string",
         }
-    ]
+      ]
+    },
+    {
+      name: "codeLink",
+      title:"Code Link",
+      type: "url",
+    },
+    {
+      name: "projectLink",
+      title:"Project Link",
+      type: "url",
+    },
+    {
+      name: "colour",
+      title: "Colour",
+      type: "string",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [{ type: "block" }]
+    },
+    {
+      name: "technologies",
+      title: "Technologies used",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          fields: [
+            {
+              name: "name",
+              title: "Name",
+              type: "string",
+            },
+            {
+              name: "link",
+              title: "Link",
+              type: "string",
+            }
+          ]
+        },
+      ],
+    },
+  ]
 };
