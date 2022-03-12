592817dfb7534d59ebf4319891fa1a61b0231d96
Andykbt <andy.truong2001@gmail.com>
2022-03-12 15:59:02 +1100

    indexAll endpoint

---
+++ web/pages/api/indexAll.ts
@@ -0,0 +1,28 @@
+import { algoliaClient } from "@website-v3/web/algolia";
+import { SanityClient } from "../../sanity";
+
+const query = `
+  *[ _type in ['article', 'project']] {
+    "objectID": _id,
+    _type,
+    _createdAt,
+    "imageUrl": image.asset -> url,
+    projectLink,
+    "slug": slug.current
+  }`;
+
+export default async function handler(req: any, res: any) {
+  const documents = await SanityClient.fetch(query);
+
+  const index = algoliaClient.initIndex(process.env.ALGOLIA_INDEX || "");
+
+  try {
+    console.time(`Saving ${documents.length} documents to index: `);
+    await index.saveObjects(documents);
+    console.timeEnd(`Saving ${documents.length} documents to index: `);
+
+    res.status(200).json("Success");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
