67f783ace3605716bc269d4ad45ab9d0c352477b
Andykbt <andy.truong2001@gmail.com>
2022-03-13 12:09:41 +1100

    article update handler

---
+++ web/pages/api/updateArticle.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { SanityClient } from "@website-v3/web/sanity";
+import { algoliaClient } from "@website-v3/web/algolia";
+import indexer from "sanity-algolia";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const index = algoliaClient.initIndex(process.env.ALGOLIA_INDEX || "");
+
+  const sanityAlgolia = indexer(
+    {
+      article: {
+        index: index,
+        projection: `{
+          _type,
+          _createdAt,
+          "imageUrl": image.asset -> url,
+          projectLink,
+          "slug": slug.current
+        }`,
+      },
+    },
+    // Serializer function to manipulate documents
+    (document) => document,
+    // Visibility function to determine which documents to be included
+  );
+  return sanityAlgolia
+    .webhookSync(SanityClient, req.body as any)
+    .then(() => {
+      res.status(200).json("Success");
+    })
+    .catch(() => {
+      res.status(500).json("Something went wrong");
+    });
+}
