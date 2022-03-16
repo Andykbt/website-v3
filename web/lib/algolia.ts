7f8c571c9c92a1a99ac444c10a64840c0462ce73
Andykbt <andy.truong2001@gmail.com>
2022-03-16 21:06:09 +1100

    algolia config

--- web/lib/algolia.ts
+++ web/lib/algolia.ts
@@ -2,7 +2,7 @@ import algoliasearch from "algoliasearch";
 
 export const algoliaClient = algoliasearch(
   process.env.ALGOLIA_APP_ID || "",
-  process.env.ALGOLIA_ADMIN_ID || "",
+  process.env.ALGOLIA_SEARCH_KEY || "",
 );
 
 export const algoliaIndex = algoliaClient.initIndex(process.env.ALGOLIA_INDEX || "");
