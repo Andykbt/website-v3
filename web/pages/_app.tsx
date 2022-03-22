ba560dcfa68f8c6145f3973a83020c1e547641bb
Andykbt <andy.truong2001@gmail.com>
2022-03-22 18:14:32 +1100

    added progress to root component

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "@website-v3/web/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Header } from "../components";
+import { Header } from "@website-v3/web/components";
 import { Url } from "@website-v3/web/constants/types";
 import { RecoilRoot } from "recoil";
+import { Progress } from "@website-v3/web/components/progress";
 
 const navItems: Url[] = [
   {
@@ -28,9 +29,10 @@ const navItems: Url[] = [
   },
 ];
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {  
   return (
     <RecoilRoot>
+      <Progress />
       <Header navItems={navItems} />
       <Component {...pageProps} />
     </RecoilRoot>
