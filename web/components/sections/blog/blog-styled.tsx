dbb1c732681f8246d7299cb61df8c92c04cac8b3
Andykbt <andy.truong2001@gmail.com>
2022-05-03 03:55:38 +1000

    fixed image hightlighting issue

--- web/components/sections/blog/blog-styled.tsx
+++ web/components/sections/blog/blog-styled.tsx
@@ -45,6 +45,15 @@ export const ImageWrapper = styled.div`
   height: 500px;
   position: relative;
   cursor: none;
+
+  & img {
+    user-drag: none;
+    -webkit-user-drag: none;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+  }
 `;
 
 export const MoreArticlesSoon = styled.div`
